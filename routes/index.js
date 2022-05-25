44e710ac69cef6fa3bfe60e55cf8f4da215e5340
yc62897441 <yc62897441@gmail.com>
2022-05-25 12:56:50 +0800

    routers add middleware authenticated

--- routes/index.js
+++ routes/index.js
@@ -41,32 +41,32 @@ module.exports = (app, passport) => {
   app.get('/admin/users', isAdminUser, (req, res) => { })
 
   // 一般用戶介面 A3-developer in charge
-  app.get('/tweets', isUser, tweetController.getTweets)
-  app.post('/tweets', isUser, tweetController.postTweet)
-  app.get('/tweets/:id', isUser, tweetController.getTweet)
-  app.get('/tweets/:id/replies', isUser, (req, res) => { })
-  app.post('/tweets/:id/replies', isUser, tweetController.postReply)
-  app.post('/tweets/:id/like', isUser, tweetController.likeTweet)
-  app.post('/tweets/:id/unlike', isUser, tweetController.unlikeTweet)
+  app.get('/tweets', authenticated, isUser, tweetController.getTweets)
+  app.post('/tweets', authenticated, isUser, tweetController.postTweet)
+  app.get('/tweets/:id', authenticated, isUser, tweetController.getTweet)
+  app.get('/tweets/:id/replies', authenticated, isUser, (req, res) => { })
+  app.post('/tweets/:id/replies', authenticated, isUser, tweetController.postReply)
+  app.post('/tweets/:id/like', authenticated, isUser, tweetController.likeTweet)
+  app.post('/tweets/:id/unlike', authenticated, isUser, tweetController.unlikeTweet)
 
   // 一般用戶使用者資訊介面 A3-developer in charge
-  app.get('/users/:id/tweets', isUser, userController.getTweets)
-  app.get('/users/:id/replies', isUser, userController.getReplies)
-  app.get('/users/:id/followings', isUser, (req, res) => { })
-  app.get('/users/:id/followers', isUser, (req, res) => { })
-  app.get('/users/:id/likes', isUser, userController.getLikes)
+  app.get('/users/:id/tweets', authenticated, isUser, userController.getTweets)
+  app.get('/users/:id/replies', authenticated, isUser, userController.getReplies)
+  app.get('/users/:id/followings', authenticated, isUser, (req, res) => { })
+  app.get('/users/:id/followers', authenticated, isUser, (req, res) => { })
+  app.get('/users/:id/likes', authenticated, isUser, userController.getLikes)
 
   // 使用者資訊介面修改 in charge 未定
-  app.get('/api/users/:id', isUser, (req, res) => { })
-  app.put('/api/users/:id', isUser, userController.putUserApi)
+  app.get('/api/users/:id', authenticated, isUser, (req, res) => { })
+  app.put('/api/users/:id', authenticated, isUser, userController.putUserApi)
 
   // 追蹤功能 in charge 未定
-  app.post('/followships/:id', isUser, followshipController.follow)
-  app.delete('/followships/:id', isUser, followshipController.unfollow)
+  app.post('/followships/:id', authenticated, isUser, followshipController.follow)
+  app.delete('/followships/:id', authenticated, isUser, followshipController.unfollow)
 
   // 設定/更改個人資料
-  app.get('/setting', isUser, settingController.settingPage)
-  app.put('/setting', isUser, settingController.setting)
+  app.get('/setting', authenticated, isUser, settingController.settingPage)
+  app.put('/setting', authenticated, isUser, settingController.setting)
 
   // 註冊、登出、一般用戶登入 A2-developer in charge
   app.get('/signin', userController.signinPage)
